82d8efc1aeb3c0af3de1f0e5c1476bd5c959fdcf
chistakaribsky <karybsky@gmail.com>
2024-02-29 18:20:17 +0200

    added 'goods' app to project

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         "title": "Neekea - main page", 
+        "body_class_name": "index-body",
         "header": "NEEKEA furniture store"
         }
 
@@ -13,7 +14,7 @@ def index(request):
 
 def about(request):
     context = {
-        "title": "Neekea - about us", 
+        "title": "Neekea - about us",
         "header": "About us",
         "text_on_page": "About usAbout usAbout usAbout usAbout usAbout usAbout us"
         }
