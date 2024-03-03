5ba33099886d442a0ca74d927fecfd876b7a16e2
chistakaribsky <karybsky@gmail.com>
2024-03-03 02:59:33 +0200

    added functionality to work with products data from database not from hardcoded temporary list

--- goods/models.py
+++ goods/models.py
@@ -4,12 +4,14 @@ from django.core.validators import MaxValueValidator
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name="Category name")
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
 
     class Meta:
-        db_table = 'category'
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
+        db_table = "category"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
     def __str__(self) -> str:
         return self.name
@@ -17,18 +19,38 @@ class Categories(models.Model):
 
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name="Product name")
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
     description = models.TextField(blank=True, null=True, verbose_name="Description")
-    image = models.ImageField(upload_to="goods_images", blank=True, null=True, verbose_name="Photo")
-    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="Price")
-    discount = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(99)], verbose_name="Discount amount in %")
+    image = models.ImageField(
+        upload_to="goods_images", blank=True, null=True, verbose_name="Photo"
+    )
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Price"
+    )
+    discount = models.PositiveIntegerField(
+        default=0,
+        validators=[MaxValueValidator(99)],
+        verbose_name="Discount amount in %",
+    )
     quantity = models.PositiveIntegerField(default=0, verbose_name="Quantity")
-    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Category')
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.CASCADE, verbose_name="Category"
+    )
 
     class Meta:
-        db_table = 'product'
-        verbose_name = 'Product'
-        verbose_name_plural = 'Products'
+        db_table = "product"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
 
     def __str__(self) -> str:
         return f"{self.name}, {self.quantity} pcs"
+
+    def display_id(self):
+        return f"{self.id:05}"
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+        return self.price
